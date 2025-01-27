f59b54b750a61a5d3fa2c93ae18e30d54e7ab19b
Alberto <mantillaalberto@americana.edu.co>
2025-01-27 16:19:36 -0500

    Prueba de guardar pedido

--- js/app.js
+++ js/app.js
@@ -47,11 +47,13 @@ if (window.location.pathname.endsWith('/') || window.location.pathname.includes(
   
 if (window.location.pathname.includes('menu')) {
 
+    var platos = [];
+
    function mostrarPedido(){
         const infoDiv = document.getElementById('infoDiv');
         const overlayDiv = document.getElementById('overlay');
+        document.querySelector('.menu__opciones').reset();
         if(infoDiv && overlayDiv){
-           
             infoDiv.style.display = 'flex';
             infoDiv.style.justifyContent ='center';
             overlayDiv.style.display = 'block';
@@ -60,11 +62,63 @@ if (window.location.pathname.includes('menu')) {
    function cerrarPedido(){
         const infoDiv = document.getElementById('infoDiv');    
         const overlayDiv = document.getElementById('overlay');
-
-        if(infoDiv && overlayDiv){
+        const pedidos = document.getElementById('Listapedidos');
+        
+        if(infoDiv && overlayDiv && pedidos){
             infoDiv.style.display = 'none';
             overlayDiv.style.display= 'none';
-            
+            pedidos.style.display = 'none';
         }
+
+      
    }
+
+   var pedido = [];
+   document.getElementById('guardar-pedido').addEventListener('click', function () {
+       const radioArroz = document.querySelector('input[name="arroz"]:checked');
+       const radioEnsaladas = document.querySelector('input[name="ensalada"]:checked');
+       const radioGranos = document.querySelector('input[name="granos"]:checked');
+       const radioSopas= document.querySelector('input[name="sopa"]:checked');
+       const plato = document.getElementById('nombre__plato').textContent;
+        if(radioArroz && radioEnsaladas && radioGranos && radioSopas){
+            let nuevoPedido ={
+                plato: plato,
+                arroz: radioArroz.value,
+                ensalada: radioEnsaladas.value,
+                granos: radioGranos.value,
+                sopa: radioSopas.value
+            }
+            pedido.push(nuevoPedido);
+        }
+    });
+   
+   
+    function listarPedidos(){
+        const pedidos = document.getElementById('Listapedidos');
+        const overlayDiv = document.getElementById('overlay');
+        if(pedidos && overlayDiv){
+            pedidos.style.display = 'flex';
+            pedidos.style.justifyContent ='center';
+            overlayDiv.style.display = 'block';
+        }
+        console.log(pedido);
+        const verOrden = document.getElementById('enviarOrden');
+        verOrden.innerHTML = '';
+
+        pedido.forEach((p, index) => {
+            const pedidoHTML = `
+                <div class="pedido" style="display:block">
+                    <h4>Pedido ${index + 1}</h4>
+                    <p><strong>Plato:</strong> ${p.plato}</p>
+                    <p><strong>Arroz:</strong> ${p.arroz}</p>
+                    <p><strong>Ensalada:</strong> ${p.ensalada}</p>
+                    <p><strong>Grano:</strong> ${p.granos}</p>
+                    <p><strong>Sopa:</strong> ${p.sopa}</p>
+                    <hr>
+                </div>
+            `;
+            verOrden.innerHTML += pedidoHTML;
+        });
+   } 
+
 }
